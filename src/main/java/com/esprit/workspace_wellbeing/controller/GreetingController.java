3d95320699216b991edb373e332550df9ee849ee
Mahdi <mohamedmahdi.jendoubi@esprit.tn>
2022-07-21 16:20:43 +0200

    init chat module

---
+++ src/main/java/com/esprit/workspace_wellbeing/controller/GreetingController.java
@@ -0,0 +1,22 @@
+package com.esprit.workspace_wellbeing.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+import com.esprit.workspace_wellbeing.entity.Greeting;
+import com.esprit.workspace_wellbeing.entity.HelloMessage;
+
+@Controller
+public class GreetingController {
+
+
+	@MessageMapping("/hello")
+	@SendTo("/topic/greetings")
+	public Greeting greeting(HelloMessage message) throws Exception {
+		Thread.sleep(1000); // simulated delay
+		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+	}
+
+}
